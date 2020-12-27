ff334d9cd0753f53b7dc6b862ddc95c56cdf2b3f
Laxmikant Bhole <laxmikantbhole88@gmail.com>
2020-12-27 13:01:35 +0530

    removed unused imports

--- src/com/devmode/springdemo/HelloSpringApp.java
+++ src/com/devmode/springdemo/HelloSpringApp.java
@@ -1,34 +1,28 @@
 package com.devmode.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-//		// load the spring configuration file
-//		ClassPathXmlApplicationContext context = 
-//				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
-		AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-		
-		// retrieve bean from spring container
-		Animal animal = configApplicationContext.getBean("myHorse", Animal.class);
-		
-		// call methods on the bean
-		System.out.println(animal.getSpeed());
-		
-		// close the context
-		configApplicationContext.close();
-	}
+    // // load the spring configuration file
+    // ClassPathXmlApplicationContext context =
+    // new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    AnnotationConfigApplicationContext configApplicationContext =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+
+    // retrieve bean from spring container
+    Animal animal = configApplicationContext.getBean("myHorse", Animal.class);
+
+    // call methods on the bean
+    System.out.println(animal.getSpeed());
+
+    // close the context
+    configApplicationContext.close();
+  }
 
 }
-
-
-
-
-
 
 
